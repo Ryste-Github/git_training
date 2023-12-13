3f591dfec228e6e6c5aecc85b3f85e60b9146c86
[Ryste-Github] <[rasmusbjp@gmail.com]>
2023-12-13 08:50:20 +0000

    11

--- test_package1.R
+++ test_package1.R
@@ -1,6 +1,2 @@
 library(tidyverse) # very good library
-<<<<<<< HEAD
-library(postgres) # good library this is
-=======
-library(postgres) # very good library
->>>>>>> 03fc29734f899d08cde3e22782442a43ec1c0575
+library(postgres) # very good library this is
